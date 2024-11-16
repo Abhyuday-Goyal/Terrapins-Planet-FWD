3aaa27dcccaa0dcfecc95df8f5d8390d888228e8
shasshank12 <shasshank12@gmail.com>
2024-11-16 15:19:35 -0500

    Green

--- frontend/src/components/items/Button.jsx
+++ frontend/src/components/items/Button.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
-const Button = ({ onClick, children, className, href }) => {
-  const Component = href ? Link : 'button';
-  const props = href ? { to: href } : { onClick };
-
+const Button = ({ onClick, children, className }) => {
   return (
-    <Component
-      {...props}
-      className={`relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff] ${className}`}
+    <button
+      onClick={onClick}
+      className={`py-2 px-6 rounded-lg text-white font-medium transition-transform duration-200 ease-in-out ${className}`}
     >
-      <div className="absolute inset-0">
-        <div className="rounded-lg border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
-        <div className="rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
-        <div className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
-      </div>
-      <span>{children}</span>
-    </Component>
+      {children}
+    </button>
   );
 };
 
@@ -25,13 +16,11 @@ Button.propTypes = {
   onClick: PropTypes.func,
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  href: PropTypes.string,
 };
 
 Button.defaultProps = {
   onClick: null,
-  className: '',
-  href: null,
+  className: 'bg-earth-green hover:bg-light-green',
 };
 
 export default Button;
